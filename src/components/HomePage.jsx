e69de07f82399998f03ec3918632be10ba88ad34
Ullas Chandrakar <ullaschandrakar2404@gmail.com>
2023-12-18 21:06:32 +0530

    carousel

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Carousel from './Carousel'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className='bg-amazonclone-background'>
+        <div className='min-w-[1000px] max-w-[1400px] m-auto bg-purple-300'>
+            Testing
+            <Carousel />
+        </div>
+    </div>
   )
 }
 
