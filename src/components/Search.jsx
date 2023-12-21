243ce78705878ff014a3718ee48b47f3d99d68d9
Ullas Chandrakar <ullaschandrakar2404@gmail.com>
2023-12-21 20:58:45 +0530

    search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { callAPI } from '../utils/CallApi';
+import { useNavigate, createSearchParams } from "react-router-dom";
 
 const Search = () => {
+
+  const [suggestions, setSuggestions] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [category, setCategory] = useState("All");
+
+  const navigate = useNavigate();
+
+  const getSuggestions = () => {
+    callAPI(`data/suggestions.json`).then((suggestionResults) => {
+      setSuggestions(suggestionResults);
+    });
+  };
+
+  const onHandleSubmit = (e) => {
+    e.preventDefault();
+
+    navigate({
+      pathname: "search",
+      search: `${createSearchParams({
+        category: `${category}`,
+        searchTerm: `${searchTerm}`,
+      })}`,
+    });
+
+    setSearchTerm("");
+    setCategory("All");
+  };
+
+
+  useEffect(() => {
+    getSuggestions();
+  }, []);
   return (
     <div className='w-[100%]'>
         <div className='flex items-center h-10 bg-amazonclone-yellow rounded'>
             <select
+                 onChange={(e) => setCategory(e.target.value)}
             className="p-2 bg-gray-300 text-black border text-xs xl:text-sm"
             >
                 <option>All</option>
@@ -16,11 +51,37 @@ const Search = () => {
                 <option>Home</option>
                 <option>Mobiles</option>
             </select>
-            <input className='flex grow items-center h-[100%] rounded-l text-black'  type='text'/>
-            <button className='w-[40px]'>
+            <input className='flex grow items-center h-[100%] rounded-l text-black'  
+                type='text' value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <button  onClick={onHandleSubmit} className='w-[40px]'>
                 <MagnifyingGlassIcon className='h-[25px] m-auto stroke-slate-900'/>
             </button>
         </div>
+        {suggestions && (
+        <div className="bg-white text-black w-full z-40 absolute">
+          {suggestions
+            .filter((suggestion) => {
+              const currentSearchTerm = searchTerm.toLowerCase();
+              const title = suggestion.title.toLowerCase();
+              return (
+                currentSearchTerm &&
+                title.startsWith(currentSearchTerm) &&
+                title !== currentSearchTerm
+              );
+            })
+            .slice(0, 10)
+            .map((suggestion) => (
+              <div
+                key={suggestion.id}
+                onClick={() => setSearchTerm(suggestion.title)}
+              >
+                {suggestion.title}
+              </div>
+            ))}
+        </div>
+      )}
     </div>
   )
 }
